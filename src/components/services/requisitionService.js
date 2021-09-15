fdf58fe3612af470af242e3cebba31c07651075b
Kaveen LK <kmklaksitha@gmail.com>
2021-09-15 18:15:29 +0530

    requisition service completed

--- src/components/services/requisitionService.js
+++ src/components/services/requisitionService.js
@@ -2,6 +2,22 @@ import axios from "axios";
 
 const HOST = "http://localhost:4000";
 
+//to add a requisition
+export const addRequisition = async () => {
+    try {
+        const response = await axios.get(`${HOST}/requisition/addRequisition`);
+        console.log("dataaaaaaaa", response)
+        return {
+            ok: true
+        }
+    } catch (error) {
+        return {
+            ok: false
+        }
+
+    }
+}
+
 //to retrieve all requisition records
 export const getAllRequisition = async () => {
     try {
@@ -16,5 +32,47 @@ export const getAllRequisition = async () => {
             ok: false
         }
 
+    }
+}
+
+//to update the status of the requisition record
+export const updateRequisitionStatus = async (reqID, payload) => {
+    try {
+        const response = await axios.put(`${HOST}/requisition/updateRequisition/${reqID}`, payload);
+        return {
+            ok: true,
+        };
+    } catch (error) {
+        return {
+            ok: false, err: error.response.data.status
+        };
+    }
+}
+
+//to delete a requisition record
+export const deleteRequisitionRecord = async (payload) => {
+    try {
+        const response = await axios.post(`${HOST}/requisition/deleteRequisition`, payload);
+        return {
+            ok: true,
+        };
+    } catch (error) {
+        return {
+            ok: false, err: error.response.data.status
+        };
+    }
+}
+
+//to view one requisition record
+export const getOneRequisitionRecord = async (reqID) => {
+    try {
+        const response = await axios.get(`${HOST}/requisition/getRequisitionByID/${reqID}`);
+        return {
+            ok: true,
+        };
+    } catch (error) {
+        return {
+            ok: false, err: error.response.data.status
+        };
     }
 }
