3bfe35684487f275376c1285805af12a307c7e97
devin <devinmunasinghe@gmail.com>
2021-09-17 18:13:03 +0530

    draft modal started

--- src/components/views/requisitions/requisitionDraftList.js
+++ src/components/views/requisitions/requisitionDraftList.js
@@ -2,10 +2,16 @@ import { React, useState, useEffect } from 'react'
 
 import MaterialTable from "material-table";
 import HeaderForReq from "./headerForReq"
+import { Modal } from "react-bootstrap";
+
+import {draftModal} from "./draftViewModal"
 
 function RequisitionDraftList() {
 
     const [prList, setPrList] = useState([]);
+
+    const [modalStateUpdate, setModalStateUpdate] = useState(false);
+    const [currentOrderUpdate, setCurrentOrderUpdate] = useState();
 
     useEffect(() => {
         // getAllDrafts().then((res) => {
@@ -46,20 +52,35 @@ function RequisitionDraftList() {
                             actionsColumnIndex: -1,
                             exportButton: true,
                         }}
-                    // actions={[
-                    //     {
-                    //         icon: () => (
-                    //             <button class="btn btn-sm btn-warning">view</button>
-                    //         ),
-                    //         onClick: (event, rowData) => {
-                    //             setCurrentProductUpdate(rowData);
-                    //             setModalStateUpdate(true);
-                    //         },
-                    //     },
-                    // ]}
+                    actions={[
+                        {
+                            icon: () => (
+                                <button class="btn btn-sm btn-warning">view</button>
+                            ),
+                            onClick: (event, rowData) => {
+                                setCurrentOrderUpdate(rowData);
+                                setModalStateUpdate(true);
+                            },
+                        },
+                    ]}
                     />
                 </table>
             </div>
+            <Modal show={modalStateUpdate}
+                onHide={() => setModalStateUpdate(false)}
+                size="lg"
+                aria-labelledby="containeed-modal-title-vcenter"
+                centered
+
+
+            >
+                <draftModal
+                    data={currentOrderUpdate}
+                    onHide={() => setModalStateUpdate(false)}
+
+                />
+
+            </Modal>
 
 
         </div>
