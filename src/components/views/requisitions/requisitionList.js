8ae07ad570180a505b3756f5a2d5e8fabdc6c20e
Kaveen LK <kmklaksitha@gmail.com>
2021-09-17 16:40:26 +0530

    requisitions views updated

---
+++ src/components/views/requisitions/requisitionList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderForReq from "./headerForReq"
+
+function requisitionList() {
+    return (
+        <div className="component-body">
+            <HeaderForReq></HeaderForReq>
+
+
+        </div>
+    )
+}
+
+export default requisitionList
